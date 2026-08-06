246a0806988cead9d75ec1d7b6ea348eaa1d4742
Penguin Duck <patrickd9900@gmail.com>
2026-08-06 02:12:55 -0500

    added buffered writing

--- main.c
+++ main.c
@@ -2,20 +2,26 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 // ascii bits [4:0] represent numerical order of alphabet
-#define CTRL(k) (k & 0x1f)
+#define CTRL_KEY(k) (k & 0x1f)
 
 struct editorConfig {
-  struct termois orig_termios;
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
 };
 
 struct editorConfig E;
 
+/** terminal **/
+
 void die(const char *s) {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
+ write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[1;1H", 6);
 
   // perror looks at global error variable and prints a message
@@ -81,27 +87,94 @@ char editorReadKey() {
   return c;
 }
 
-void editorDrawRows() {
-  for (int y = 0; y < 24; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999B\x1b[999C", 12) != 12) return -1;
+    return getCursorPosition(&E.screenrows, &E.screencols);
+  }
+  else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
   }
 }
 
-void editorRefreshScreen() {
-  // https://vt100.net/docs/vt100-ug/chapter3.html#ED
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  // https://vt100.net/docs/vt100-ug/chapter3.html#CUP
-  write(STDOUT_FILENO, "\x1b[1;1H", 6);
+/** append buffer **/
 
-  editorDrawRows();
-  write(STDOUT_FILENO, "\x1b[1;1H", 6);
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
 }
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+
+/** output **/
+
+void editorDrawRows(struct abuf *ab) {
+  for (int y = 0; y < E.screenrows - 1; y++) {
+    abAppend(ab, "~\r\n", 3);
+  }
+  // I found that the final \n will push past the screen and then our
+  // tildes will escape being cleared by the J escape sequence
+  abAppend(ab, "~", 1);
+}
+
+void editorRefreshScreen() {
+  struct abuf ab = ABUF_INIT;
+
+  // https://vt100.net/docs/vt100-ug/chapter3.html#ED
+  abAppend(&ab, "\x1b[2J", 4);
+  // https://vt100.net/docs/vt100-ug/chapter3.html#CUP
+  abAppend(&ab, "\x1b[1;1H", 6);
+
+  editorDrawRows(&ab);
+
+  abAppend(&ab, "\x1b[1;1H", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
+}
+
+/** input **/
 
 void editorProcessKeypress() {
   char c = editorReadKey();
 
   switch(c) {
-  case CTRL('q'):
+  case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[1;1H", 6);
     exit(0);
@@ -109,8 +182,15 @@ void editorProcessKeypress() {
   }
 }
 
+/** init **/
+
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die ("getWindowSize");
+}
+
 int main(void) {
   enableRawMode();
+  initEditor();
   
   while (1) {
     editorRefreshScreen();
