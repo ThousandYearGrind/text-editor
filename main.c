c5c00fa7b35e405297c311b73b3340f2467167dc
ThousandYearGrind <patrickd9900@gmail.com>
2026-08-17 00:56:00 -0400

    modified append row to insert row, updated all usages of append row

--- main.c
+++ main.c
@@ -244,10 +244,12 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+	if (at < 0 || at > E.numrows) return;
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+	memmove(E.row + at + 1, E.row + at, sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -302,7 +304,7 @@ void editorRowDelChar(erow *row, int at) {
 /** editor operations **/
 
 void editorInsertChar(int c) {
-	if (E.cy == E.numrows) editorAppendRow("", 0);
+	if (E.cy == E.numrows) editorInsertRow(0, "", 0);
 	editorRowInsertChar(&E.row[E.cy], E.cx, c);
 	E.cx++;
 	E.dirty++;
@@ -396,7 +398,7 @@ void editorOpen(char *filename) {
   while ((linelen = getline(&line, &linecap, fp)) != -1) {
     while (linelen > 0 && (line[linelen - 1] == '\n' || 
       line[linelen - 1] == '\r')) linelen--;
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
